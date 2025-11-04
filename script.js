64fc83a44f77de0fd0786cfac271c731c6b85cc8
Prem Kumar <prem144047@gmail.com>
2025-11-04 22:48:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-//your JS code here. If required.
 document.querySelectorAll('.code').forEach((input, index, inputs) => {
     input.addEventListener('input', (e) => {
-        if (e.target.value.length === 1 && index < inputs.length - 1) {
-            inputs[index + 1].focus();
+        // Move to the next field if the current field is filled
+        if (e.target.value.length === 1) {
+            if (index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
         }
     });
 
     input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && index > 0 && input.value === '') {
+        // Move to the previous field if backspace is pressed and the current field is empty
+        if (e.key === 'Backspace' && input.value === '') {
+            if (index > 0) {
+                inputs[index - 1].focus();
+            }
+        }
+    });
+
+    // Automatically focus the previous field if a number is typed in the current field
+    input.addEventListener('input', (e) => {
+        if (e.target.value.length === 1 && index > 0) {
             inputs[index - 1].focus();
         }
     });
